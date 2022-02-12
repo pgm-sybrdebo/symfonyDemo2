ed7ed9e379c6708f07e6eb6eb4a0f1bb2c0ecb95
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2022-02-12 14:07:58 +0100

    created first symfony project +added first controller new and old method

---
+++ movies/src/Controller/MoviesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+  #[Route('/movies', name: 'movies')]
+  public function index(): Response
+  {
+    return $this->json([
+      'message' => 'Welcome to your new controller!',
+      'path' => 'src/Controller/MoviesController.php',
+    ]);
+  }
+
+
+  /**
+   * oldMethod
+   *
+   * @Route("/old", name="old")
+   */
+  public function oldMethod(): Response
+  {
+    return $this->json([
+      'message' => 'Old method!',
+      'path' => 'src/Controller/MoviesController.php',
+    ]);
+  }
+}
